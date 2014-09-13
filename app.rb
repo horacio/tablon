52c8ae86afbe7b12f916fd94fc57f12c7fa45fdd
Horacio Bertorello <syrii@msn.com>
2014-09-13 19:21:30 -0300

    Grab job postings from JSON file.

--- app.rb
+++ app.rb
@@ -2,23 +2,22 @@ require 'cuba'
 require 'mote'
 require 'mote/render'
 
+require 'json'
+
 Cuba.use Rack::Static, urls: ['/media']
 
 Cuba.plugin(Mote::Render)
 
 Cuba.define do
   on root do
-    render('home', jobs: fake_jobs)
+    render('home', jobs: jobs)
   end
 end
 
-def fake_jobs
-  5.times.map do
-    Struct.new(:title, :company, :description, :posted_on, :email)
-      .new("Software Engineer - Bathroom Naps",
-           "Intel",
-           "Some of the brightest minds of our generation...",
-           "2014-09-12 18:45:07",
-           "jobs+argentina@intel.com")
-  end
+def jobs
+  JSON.parse(jobs_file)
+end
+
+def jobs_file
+  File.open('data/jobs.json').read
 end
